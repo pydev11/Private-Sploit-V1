3bf369efd42f2a023e4900a94a93164f8c886132
Thankful Python <167735173+pydev11@users.noreply.github.com>
2024-12-17 14:56:00 -0500

    Delete robloxtest.lua

--- robloxtest.lua
+++
@@ -1,9 +0,0 @@
-local Players = game:GetService("Players")
-
-Players.PlayerAdded:Connect(function(Player)
-	for _, v in script.PlayerValues:GetChildren() do
-		v:Clone().Parent = Player
-	end
-end)
-
-local function Base64(Base4, Dylibs, Args1, Args2, Args3, Args4)
