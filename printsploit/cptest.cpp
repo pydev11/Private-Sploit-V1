cb59f1afb0b10dd285835e0c75fcb6be4f5032a4
Thankful Python <167735173+pydev11@users.noreply.github.com>
2024-12-22 16:24:53 -0500

    Update cptest.cpp

--- printsploit/cptest.cpp
+++ printsploit/cptest.cpp
@@ -1,2 +1,8 @@
-#inclue <iostream>
-#include <mach-o> // testing this 
+#include <iostream>
+#include <mach-o/loader.h> // Fixed header include for Mach-O
+
+int extra() {
+    std::cout << "Print Test";
+    for extra do 
+    return 0;
+} // not being worked on for now
