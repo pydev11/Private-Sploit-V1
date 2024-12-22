d8b5ef4b774e8cdb7b06f60071869340792c4d5d
Thankful Python <167735173+pydev11@users.noreply.github.com>
2024-12-22 18:57:35 -0500

    Update hooks.cpp

--- hooks.cpp
+++ hooks.cpp
@@ -1,1 +1,5 @@
-#include <iostream>
+#include <thread>
+
+std::this_thread::sleep_for(std::chrono::seconds(20));
+    typedef int (*printdef)(int, const char*, ...);
+    auto printfunction = reinterpret_cast<printdef>(_dyld_get_image_vmaddr_slide(0) + (uintptr_t)offset);
